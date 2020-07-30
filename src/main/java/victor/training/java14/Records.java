2c45c0a63873756df4d6600ed1a79afea755df3f
Victor Rentea <victorrentea@gmail.com>
2020-07-30 17:52:23 +0300

    Records init

---
+++ src/main/java/victor/training/java14/Records.java
@@ -0,0 +1,15 @@
+package victor.training.java14;
+
+public class Records {
+   public static void main(String[] args) {
+      // final final final
+      // implements, abstract and extends
+      // 2nd constructor
+      // extra content
+      // override generated methods
+   }
+
+   public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
+      return start1 <= end2 && start2 <= end1;
+   }
+}
