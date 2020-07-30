569655881447150df043815e09b4552a097dfe25
Victor Rentea <victorrentea@gmail.com>
2020-07-30 18:55:49 +0300

    Streams

---
+++ src/main/java/victor/training/java11/StreamsTake.java
@@ -0,0 +1,11 @@
+package victor.training.java11;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamsTake {
+   public static void main(String[] args) {
+      List.of("","","Row1","Row2","Row3","","Row5");
+      // dropWhile
+   }
+}
