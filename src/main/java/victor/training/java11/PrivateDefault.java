494a4bb0914cbeb1a3b7c4baae4bd74dee2aaa2f
Victor Rentea <victorrentea@gmail.com>
2020-07-31 12:49:02 +0300

    default methods take1

--- src/main/java/victor/training/java11/PrivateDefault.java
+++ src/main/java/victor/training/java11/PrivateDefault.java
@@ -1,11 +1,51 @@
 package victor.training.java11;
 
+import java.util.List;
+
 public class PrivateDefault {
 
    public static void main(String[] args) {
       //
+      List<String> list = List.of("a", "c");
+      list.stream();
    }
 }
+
+interface A {
+   public static final int x = 1; // always constant
+   default int m() {
+//      x = 2;
+      return 1;
+   }
+   abstract void f();
+}
+abstract class AAbstra {
+   int x = 1;
+   abstract protected void f();
+   int m() {
+      x ++;
+      return 1;
+   }
+}
+interface B {
+   default int m() {
+      return 2;
+   }
+}
+
+class AB implements A,B{
+
+   @Override
+   public int m() {
+      return A.super.m();
+   }
+
+   @Override
+   public void f() {
+
+   }
+}
+
 
 interface ICommChanel {
 }
