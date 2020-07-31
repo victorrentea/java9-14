0693fe262f2caca18072470f924cb8a8cc1bb560
Victor Rentea <victorrentea@gmail.com>
2020-07-31 12:26:31 +0300

    YOLO

--- src/main/java/victor/training/java11/TryWithResources.java
+++ src/main/java/victor/training/java11/TryWithResources.java
@@ -2,17 +2,27 @@ package victor.training.java11;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class TryWithResources {
 
    public static void main(String[] args) throws IOException {
+//      System.out.close();;
+      System.out.println("After");
       String s = """
         "f1"1, 
          """;
 
       checkLines(s.lines());
 
+      checkLines(List.of("a","b").stream());
+      checkLines(Stream.of("a","b"));
 
       Stream<String> lines = Files.lines(new File("data.txt").toPath());
 //      lines = inputStream parsing (network)
@@ -25,12 +35,23 @@ public class TryWithResources {
 //      }
    }
 
-   public static void checkLines(Stream<String> lines) {
+   // use Collection<> everytime, unless the number of lines is too big to fit memory
+   private static void checkLines(Stream<String> lines) {
+
       try (lines) {
-         if (lines.filter(s -> !s.isBlank()).count() <= 20) {
+         if (lines.filter(s -> debuggable(s)).count() <= 20) {
             throw new IllegalArgumentException("Incorrect file!");
          }
       }
 //      lines.close(); -- automatically do this in a finally {
+   }
+
+   private List<String> filterLines(List<String> initial) {
+      return initial.stream().filter(/*StringUtils::isNotBlank*/TryWithResources::debuggable).collect(toList());
+
+   }
+
+   private static boolean debuggable(String s) {
+      return !s.isBlank();
    }
 }
