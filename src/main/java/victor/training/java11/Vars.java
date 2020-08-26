8f4deed44ccc608da4fa8f951f623df14cd08dde
Victor Rentea <victorrentea@gmail.com>
2020-08-26 11:56:13 +0300

    better

--- src/main/java/victor/training/java11/Vars.java
+++ src/main/java/victor/training/java11/Vars.java
@@ -4,6 +4,7 @@ package victor.training.java11;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Predicate;
 
 public class Vars {
@@ -11,31 +12,16 @@ public class Vars {
    // use: local variab=, for index/variable
    // KO: lambdas, params, fields, var n;, = null, int[]
    // OK: "var var", var(), var.x.d
-   // WTH!: var List.of, var <>, anonym subtype
+   // WTH!: var List.of(string,float,int), var <>, anonym subtype
    // avoid: long pipeline, less readable,
    // uses: long classname, generics, try-w-res,
 
    // var for lambda args, for annotations; all or none
 
    public static void main(String[] args) {
-      Predicate<Apple> pred = (var apple) -> apple.isHeavy();
-      var a = List.of(1, 2, 3, 4, "a", 1.0);
       var map = Map.of(1, List.of("one"), 2, List.of("two"));
-      for (var entry : map.entrySet()) {
+      for (Entry<Integer, List<String>> entry : map.entrySet()) {
          System.out.println(entry.getKey());
       }
-
-
-   }
-}
-class X {
-   int x = 1;
-   public void m() {
-
-   }
-}
-class Apple {
-   boolean isHeavy() {
-      return true;
    }
 }
