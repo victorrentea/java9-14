b892230f5654c4a31eac345d74409f8a1dbcd346
Victor Rentea <victorrentea@gmail.com>
2020-09-09 14:28:28 +0300

    text blocks

--- src/main/java/victor/training/java14/Strings.java
+++ src/main/java/victor/training/java14/Strings.java
@@ -2,10 +2,27 @@ package victor.training.java14;
 
 public class Strings {
    public static void main(String[] args) {
-      String jude = "Hey Jude, don't make it bad\n" +
-                    "Take a sad song and make it better\n" +
-                    "Remember to let her into your heart\n" +
-                    "Then you can start to make it better";
+      String jude = """
+             Hey Jude, don't make it bad
+               Take a sad song and make it better
+            Remember to let her into your heart
+             Then you can start to make it better""";
+
+      System.out.println(jude);
+
+      String sqlQuery = """
+          SELECT stuff
+          FROM stuff 
+          LEFT JOIN child on child.parent_id----
+          WHERE""";
+
+      // language=json
+      String json = """
+          {
+            "field":1,
+            "two":2
+          }
+          """;
 
       // .isBlank 11
       // .indent
