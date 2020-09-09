81cc082a51010e04e18e0589f071cb2c583ff1da
Victor Rentea <victorrentea@gmail.com>
2020-09-09 11:54:15 +0300

    default methods (java8)

--- src/main/java/victor/training/java11/PrivateDefault.java
+++ src/main/java/victor/training/java11/PrivateDefault.java
@@ -4,11 +4,41 @@ public class PrivateDefault {
 
    public static void main(String[] args) {
       //
+      ICommChanel smsChannel = new SmsChannel();
+      smsChannel.sendAlert("Stay at home");
    }
 }
 
 interface ICommChanel {
+   void sendMessage(String message);
+   default void sendAlert(String message) {
+      String alert = message.toUpperCase() + "!";
+      sendMessage(alert);
+   }
 }
+
 class SmsChannel implements ICommChanel {
+   public void sendMessage(String message) {
+      // TODO SMS
+   }
+}
+
+
+// java7
+interface Interface {
+   public static final int CONSTANT = 1;
+   public abstract void m();
+   public abstract void stream(); // if List.stream were abstract, THOUSANDS of custom List impementations would have break
 
 }
+
+
+// java8
+interface Interface2 {
+   public static final int CONSTANT = 1;
+   default void m() {
+      // allow you to add behavior to all the implementers without breaking their implementation
+      //logic here calling ***SOME*** instance method in there
+   }
+}
+
--- src/main/java/victor/training/java11/StreamsTake.java
+++ src/main/java/victor/training/java11/StreamsTake.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class StreamsTake {
    public static void main(String[] args) {
