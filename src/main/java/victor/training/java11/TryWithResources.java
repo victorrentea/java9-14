919a6b0c0e58cdd893c049fad7f9bc85a75d9d25
Victor Rentea <victorrentea@gmail.com>
2020-09-09 11:34:53 +0300

    Supplier<Stream>

--- src/main/java/victor/training/java11/TryWithResources.java
+++ src/main/java/victor/training/java11/TryWithResources.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,23 +14,32 @@ import static java.util.stream.Collectors.toList;
 
 public class TryWithResources {
    public static void main(String[] args) throws IOException {
-      try (Stream<String> lines = Files.lines(new File("test.txt").toPath())) {
-         checkFile(lines); // fragile: if you NEED to be sure that resource is closed, then
-         checkFile2(lines);
-      }
+      checkFile(TryWithResources::openFile); // fragile: if you NEED to be sure that resource is closed, then
+      checkFile2(TryWithResources::openFile);
    }
 
+   public static Stream<String> openFile() {
+      try {
+         return Files.lines(new File("test.txt").toPath());
+      } catch (IOException e) {
+         throw new RuntimeException(e);
+      }
+   }
 
    /// this function is in a far-away class
 
-   public static void checkFile(Stream<String> lines) {
-      if (lines.filter(s -> !s.isBlank()).count() >= 20) {
-         throw new IllegalArgumentException("Incorrect file!");
+   public static void checkFile(Supplier<Stream<String>> linesSupplier) {
+      try (Stream<String> lines = linesSupplier.get()) {
+         if (lines.filter(s -> !s.isBlank()).count() >= 20) {
+            throw new IllegalArgumentException("Incorrect file!");
+         }
       }
    }
-   public static void checkFile2(Stream<String> lines) {
-      if (lines.filter(s -> !s.isBlank()).count() > 2) {
-         throw new IllegalArgumentException("Incorrect file!");
+   public static void checkFile2(Supplier<Stream<String>> linesSupplier) {
+      try (Stream<String> lines = linesSupplier.get()) {
+         if (lines.filter(s -> !s.isBlank()).count() > 2) {
+            throw new IllegalArgumentException("Incorrect file!");
+         }
       }
    }
 }
