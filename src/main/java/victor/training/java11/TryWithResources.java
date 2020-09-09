1af2889363e4489ec5ae4da61876d0f1161cea1a
Victor Rentea <victorrentea@gmail.com>
2020-09-09 11:26:38 +0300

    bad design

--- src/main/java/victor/training/java11/TryWithResources.java
+++ src/main/java/victor/training/java11/TryWithResources.java
@@ -1,7 +1,10 @@
 package victor.training.java11;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.nio.file.Files;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,11 +12,16 @@ import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.toList;
 
 public class TryWithResources {
-   public void checkFile(Stream<String> lines) {
+   public static void checkFile(Stream<String> lines) {
       try (lines) {
          if (lines.filter(s -> !s.isBlank()).count() <= 20) {
             throw new IllegalArgumentException("Incorrect file!");
          }
       }
+   }
+
+   public static void main(String[] args) throws IOException {
+      Stream<String> lines = Files.lines(new File("data.txt").toPath());
+      checkFile(lines); // fragile: if you NEED to be sure that resource is closed, then
    }
 }
