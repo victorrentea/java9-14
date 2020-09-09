c6e31c0f98051b4686f1c611a668ddf921c2a6d5
Victor Rentea <victorrentea@gmail.com>
2020-09-09 15:24:02 +0300

    trick to force the compiler check of all branches

--- src/main/java/victor/training/java14/SwitchOnEnums.java
+++ src/main/java/victor/training/java14/SwitchOnEnums.java
@@ -7,7 +7,31 @@ public class SwitchOnEnums {
          case CH, EU -> 0;
          case UK -> baseAmount * .01;
          case WORLD -> baseAmount * .1;
+         case US -> 0.0;
       };
+   }
+   public void handle(FiscalZone zone) {
+      Void v = switch (zone) {
+         case CH, EU -> handleEU();
+         case UK -> handleUK();
+         case WORLD -> handleWorld();
+         case US -> null;
+      };
+   }
+
+   public Void handleEU() {
+      System.out.println("Free zone");
+      return null;
+   }
+
+   public Void handleUK() {
+      System.out.println("UK");
+      return null;
+   }
+
+   public Void handleWorld() {
+      System.out.println("Big world");
+      return null;
    }
 }
 
@@ -15,6 +39,6 @@ enum FiscalZone {
    EU,
    CH,
    UK,
-   WORLD/*,
-   US*/
+   WORLD,
+   US
 }
