edc91e8bfb734f7b914da0859b6134f2c9ed4cad
Victor Rentea <victorrentea@gmail.com>
2020-09-09 14:18:34 +0300

    Finally!! Helpful Null Pointer Exceptions

--- src/main/java/victor/training/java14/NPE.java
+++ src/main/java/victor/training/java14/NPE.java
@@ -1,17 +1,59 @@
 package victor.training.java14;
 
-// -XX:+ShowCodeDetailsInExceptionMessages
+
+class A {
+ private final B b;
+
+   A(B b) {
+      this.b = b;
+   }
+
+   public B getB() {
+      return b;
+   }
+}
+class B {
+   private final C c;
+
+   B(C c) {
+      this.c = c;
+   }
+
+   public C getC() {
+      return c;
+   }
+}
+class C {
+   private final String name;
+
+   C(String name) {
+      this.name = name;
+   }
+
+   public String getName() {
+      return name;
+   }
+}
+
+// -XX:+ShowCodeDetailsInExceptionMessages - enable this in PROD but take care not to slip exceptions
+// to the user
+ //@RestControllerAdvice or try { stuff} catch(Exception ex) {ex.log...}
+
 public class NPE {
    public static void main(String[] args) {
-      output(new A(new B(new C("Halo"))));
+         output(new A(new B(new C("Halo"))));
+//         output(new A(new B(new C(null))));
+         output(new A(new B(null)));
+//         output(new A(null));
 
       //but it still doesn't solve 'returning death nulls'
-      System.out.println(nastyBizFunction(5123).repeat(2));
+//      System.out.println(nastyBizFunction(5123).repeat(2));
+      //
    }
 
 
    private static void output(A a) {
-      System.out.println(a.b().c().s().toUpperCase());
+      System.out.println(a.getB().getC().getName().toUpperCase());
    }
 
    private static String nastyBizFunction(int x) {
@@ -33,7 +75,3 @@ public class NPE {
       return null;
    }
 }
-
-record A(B b) {}
-record B(C c) {}
-record C(String s) {}
