a37b035d0525fbb89419c1e56a32afb761372872
Victor Rentea <victorrentea@gmail.com>
2020-09-09 10:45:40 +0300

    var x = lambda/met ref ==NO : these don't work because Lambdas and Method references work by asking you to define their type

--- src/main/java/victor/training/java11/Vars.java
+++ src/main/java/victor/training/java11/Vars.java
@@ -4,7 +4,9 @@ package victor.training.java11;
 
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import static java.util.Arrays.asList;
 import static java.util.Map.entry;
@@ -44,10 +46,32 @@ public class Vars {
       final var arr = new int[]{0, 2, 3};
 //      arr = new int[0];
 
+//      var s = null; - null can be anything
+
+
+      Predicate<Apple> lambda = Apple::isPoisonous;
+      Function<Apple, Boolean> lambda2 = Apple::isPoisonous;
+
+//      Object c = Apple::isPoisonous;
+//      Object c2 = (Apple apple) -> apple.isPoisonous();
+
+      // these don't work because Lambdas and Method references work by asking you to define their type
+//      var cv = Apple::isPoisonous;
+//      var cv2 = (Apple apple) -> apple.isPoisonous();
+
+      Apple discordsApple = new Apple();
+      lambda2.apply(discordsApple);
+
+      Supplier<Boolean> lambda3 = discordsApple::isPoisonous;
+      Boolean b = lambda3.get();
+
    }
 //   var vv = 1; // fields don't work with var
 
 //   public static var m(var number) {} -- no var in method signatures
-
-
+}
+class Apple {
+   public boolean isPoisonous() {
+      return true;
+   }
 }
