2761862db920c8623b36c6f74658e44e880c6007
Victor Rentea <victorrentea@gmail.com>
2020-09-09 16:15:06 +0300

    records can implement interfaces

--- src/main/java/victor/training/java14/Records.java
+++ src/main/java/victor/training/java14/Records.java
@@ -1,5 +1,8 @@
 package victor.training.java14;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Records {
    public static void main(String[] args) {
       // final final final
@@ -7,32 +10,44 @@ public class Records {
       // 2nd constructor
       // extra content
       // override generated methods
+      System.out.println(new Interval(1, 4));
+      Set<Interval> objects = new TreeSet<>();
+      objects.add(new Interval(2, 7));
+      objects.add(new Interval(1, 3));
+      System.out.println(objects);
    }
 
    public static boolean intervalsIntersect(Interval interval1, Interval interval2) {
-//      return interval1.start() <= interval2.end() &&
-//             interval2.start() <= interval1.end();
       return interval1.intersects(interval2);
    }
 }
-
-record Interval(int start, int end) {
+ // [1,3]   <   [2,7]
+record Interval(int start, int end) implements Comparable<Interval> {
    public boolean intersects(Interval other) {
       return start <= other.end && other.start <= end;
    }
-}
+   public int endExclusive() {
+      return end + 1;
+   }
+
+    @Override
+    public int compareTo(Interval o) {
+       return Integer.compare(start, o.start);
+    }
+ }
+
 // X add more methods
+// X more than 1 param
+// YES change the generated methods ?
 
 
 // extends // abstract records
 // comparable (implementing stuff)
-// framework integration (mybatis, jackson)
-// change the generated methods ?
 // setters ?
-// more than 1 param
 // inner classes
 // anonymous records
 // constructorS  - overloaded constructors + logic in the constructor (eg validation)
+// framework integration (mybatis, jackson)
 
 
 // Vreti o discutie despre imutabilitate?
