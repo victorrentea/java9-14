e7c7611e06c06bfdeca7e21acce431141185edd4
Victor Rentea <victorrentea@gmail.com>
2020-09-09 12:22:52 +0300

    I'm Sorry; Optional.or

--- src/main/java/victor/training/java11/Optionals.java
+++ src/main/java/victor/training/java11/Optionals.java
@@ -8,12 +8,30 @@ import static java.util.Optional.of;
 
 public class Optionals {
    public static void main(String[] args) {
+      System.out.println(getStreetNumber().flatMap(ss -> parseIfPossible(ss)));
+
       Optional<String> opt = Math.random() < 0.5 ? of("a") : empty();
 
-      String s = opt.orElseGet(() -> expensiveBackup()); // better
+
+//      String s = opt.orElseGet(() -> expensiveBackup()); // better -- calls it only if required
+//      or === flatMap + orElseGet
+      String s = opt.or(() -> expensiveBackup()).orElse(""); // better
 //      String s = opt.orElse(expensiveBackup()); //take 1 second no matter what
 
       System.out.println("Result = " + s);
+
+   }
+
+   public static Optional<String> getStreetNumber() {
+      return of("91");
+   }
+
+   public static Optional<Integer> parseIfPossible(String s) {
+      if (s.matches("\\d+")) {
+         return of(Integer.parseInt(s));
+      } else {
+         return empty();
+      }
    }
 
 //   public static void downloadBloodExamResults(Supplier<authorization> authorizer) {
@@ -22,7 +40,7 @@ public class Optionals {
 //      }
 //   }
 
-   public static String expensiveBackup() { // TODO what if this returns Optional<>
+   public static Optional<String> expensiveBackup() { // TODO what if this returns Optional<>
       System.out.println("Perform expensive operation to get a default");
       try {
          Thread.sleep(1000); // REST, File, DB
@@ -30,6 +48,6 @@ public class Optionals {
          e.printStackTrace();
       }
       System.out.println("Done");
-      return "backup";
+      return Optional.of("backup");
    }
 }
