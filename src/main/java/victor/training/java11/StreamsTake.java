f05af36c096bb59edf71d11c70e39e663d83c520
Victor Rentea <victorrentea@gmail.com>
2020-09-09 11:42:48 +0300

    take/drop While

--- src/main/java/victor/training/java11/StreamsTake.java
+++ src/main/java/victor/training/java11/StreamsTake.java
@@ -1,11 +1,18 @@
 package victor.training.java11;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamsTake {
    public static void main(String[] args) {
-      List.of("","","Row1","Row2","Row3","","Row5");
-      // dropWhile
+      List<String> strings = List.of("", "", "Row1", "Row2", "Row3", "", "Row5");
+
+      Object[] all = strings.stream().dropWhile(String::isBlank).toArray();
+//      Object[] all = strings.stream().filter(s -> !s.isEmpty()).toArray();
+
+      System.out.println(Arrays.toString(all));
+      IntStream.of(1,3,5,7,8,9,10).takeWhile(n -> n%2 ==1).forEach(System.out::println);
    }
 }
