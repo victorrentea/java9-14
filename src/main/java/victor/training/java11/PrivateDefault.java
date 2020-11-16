83fae1a9735f66e57d854a29980e94754ec7d6e5
Victor Rentea <victorrentea@gmail.com>
2020-11-16 11:01:20 +0200

    Java7: partajezi metode implementate comune intre subtipuri. Aceasta met sendAlert este utila clientilor API-ului. Este o imbogatire a interfetei, fara sa ceara implementatorilor se def acea metoda comuna

--- src/main/java/victor/training/java11/PrivateDefault.java
+++ src/main/java/victor/training/java11/PrivateDefault.java
@@ -4,11 +4,30 @@ public class PrivateDefault {
 
    public static void main(String[] args) {
       //
+
    }
 }
 
 interface ICommChanel {
+   void sendMessage(String message);
+   void sendAlert(String alert);
 }
-class SmsChannel implements ICommChanel {
 
+abstract class AbstractChannel implements ICommChanel {
+   public void sendAlert(String alert) {
+       sendMessage(alert.toUpperCase() + "!");
+   }
+}
+
+class SmsChannel extends AbstractChannel implements ICommChanel {
+   @Override
+   public void sendMessage(String message) {
+
+   }
+}
+class EmailChannel extends AbstractChannel implements ICommChanel {
+   @Override
+   public void sendMessage(String message) {
+
+   }
 }
