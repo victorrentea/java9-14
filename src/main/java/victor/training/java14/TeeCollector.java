72145093c53368cffed22a3ba204ec4a94dab8ed
Victor Rentea <victorrentea@gmail.com>
2020-11-16 13:16:35 +0200

    asta merge pentru ca instanta de Point sa pe heap, care supravietuieste terminarii metodei

--- src/main/java/victor/training/java14/TeeCollector.java
+++ src/main/java/victor/training/java14/TeeCollector.java
@@ -1,5 +1,8 @@
 package victor.training.java14;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -14,7 +17,7 @@ public class TeeCollector {
       // Java 12
       List<Integer> collect = IntStream.range(1, 10).boxed().collect(Collectors.toList());
       Long avg = collect.stream().peek(n -> {
-         total ++;
+//         total ++;
       }).collect(Collectors.teeing(summingInt(i -> i), counting(), (s, c) -> s / c));
       double asDouble = collect.stream().mapToInt(i -> i).average().getAsDouble();
 //      collect.stream().reduce(0, (acc, e) -> acc + e);
@@ -29,8 +32,9 @@ public class TeeCollector {
    }
 
    public static Supplier<Integer> getIncrementer(int a) {
-       int i = a + 1;
-       return () -> i; // lambda-ul creat are valoarea a+1 hardcodata.
+       Point p = new Point(a + 1, 0);
+//       Integer i = a + 1;
+       return () -> p.x++; // lambda-ul creat are valoarea a+1 hardcodata.
    }
 
 
