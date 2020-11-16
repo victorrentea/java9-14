ebb5b2c99bcdbafb27d67a1185b25e2cb576a5fc
Victor Rentea <victorrentea@gmail.com>
2020-11-16 12:41:08 +0200

    pattern-matchin instanceof

--- src/main/java/victor/training/java14/Instanceof.java
+++ src/main/java/victor/training/java14/Instanceof.java
@@ -6,9 +6,10 @@ public class Instanceof {
       m(false);
    }
    public static void m(Object o) {
-      if (o instanceof String) {
-         String s = (String) o;
+      if (o instanceof String s) {
          System.out.println("Someone said '" + s + "'");
+      } else if (o instanceof Integer i) {
+         System.out.println(i + 1);
       }
    }
 }
