46edeaccb5dfcfb3203f662ebd4a7969227ebf89
Victor Rentea <victorrentea@gmail.com>
2020-11-16 15:10:41 +0200

    contructorul default de recorduri il consideri ca urmeaza dupa ce s-au copiat campurile

--- src/main/java/victor/training/java14/Records.java
+++ src/main/java/victor/training/java14/Records.java
@@ -1,17 +1,13 @@
 package victor.training.java14;
 
-import lombok.Data;
-import lombok.Value;
-
-import java.util.List;
-
 public class Records {
    public static void main(String[] args) {
-      System.out.println(intervalsIntersect(new Interval(1, 3), new Interval(2, 4)));
+      Interval interval1 = new Interval(1, 3);
+      new Interval(3, 1);
+      System.out.println(interval1.intersects(new Interval(2, 4)));
 
-//      List<Interval> list;
-//      list.stream().map(Interval::getStart)
-
+      System.out.println(interval1.compareTo(new Interval(2,7)));
+      System.out.println(new Interval(1, 3).length());
       // final final final
       // implements, abstract and extends
       // 2nd constructor
@@ -19,12 +15,48 @@ public class Records {
       // override generated methods
    }
 
-   public static boolean intervalsIntersect(Interval interval1, Interval interval2) {
-      return interval1.start() <= interval2.end() && interval2.start() <= interval1.end();
-   }
 }
 
-record Interval(int start, int end) {}
+
+record Interval(int start, int end) implements Comparable<Interval> {
+
+   // subanteles
+//   public Interval(int start, int end) {
+//      this.start = start;
+//      this.end=end;
+//   }
+   public Interval {
+      System.out.println("WHAT?! constructor fara paranteze, O abominatie !");
+      // pentru ca campurile deja au fost copiat
+      if (start > end) {
+         throw new IllegalArgumentException();
+      }
+   }
+
+   public boolean intersects(Interval interval2) {
+      return start <= interval2.end && interval2.start <= end;
+   }
+
+   public int length() {
+      return end - start;
+   }
+
+   @Override
+   public int end() {
+      return end;
+   }
+
+   @Override
+   public int compareTo(Interval o) {
+      return length() - o.length();
+   }
+
+
+   @Override
+   public String toString() {
+      return "[" + start + ", " + end + ']';
+   }
+}
 
 
 //@Value
