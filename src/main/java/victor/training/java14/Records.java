acc4e00a04ee18f8871d5b9d05cafc4eb9c4da65
Victor Rentea <victorrentea@gmail.com>
2020-11-16 14:52:11 +0200

    Lombok @Value

--- src/main/java/victor/training/java14/Records.java
+++ src/main/java/victor/training/java14/Records.java
@@ -1,7 +1,17 @@
 package victor.training.java14;
 
+import lombok.Data;
+import lombok.Value;
+
+import java.util.List;
+
 public class Records {
    public static void main(String[] args) {
+      System.out.println(intervalsIntersect(new Interval(1, 3), new Interval(2, 4)));
+
+//      List<Interval> list;
+//      list.stream().map(Interval::getStart)
+
       // final final final
       // implements, abstract and extends
       // 2nd constructor
@@ -9,7 +19,21 @@ public class Records {
       // override generated methods
    }
 
-   public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-      return start1 <= end2 && start2 <= end1;
+   public static boolean intervalsIntersect(Interval interval1, Interval interval2) {
+      return interval1.getStart() <= interval2.getEnd() && interval2.getStart() <= interval1.getEnd();
    }
+}
+
+
+//1:  @Data @Getter @Setter @ToString @Equals @AllArgsConstructor @RAC
+
+//2: @Value @SneakyThrows
+
+//3: val x= 1;     x=2;   ///  = var + final
+//3: val x=new Struct();     x.setA(2);
+
+@Value
+class Interval {
+   int start;
+   int end;
 }
