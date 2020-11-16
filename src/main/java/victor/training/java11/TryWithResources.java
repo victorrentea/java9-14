1422e1104083da508f202975152aab1289279669
Victor Rentea <victorrentea@gmail.com>
2020-11-16 10:55:56 +0200

    try with resources care face .close pe o resursa care i-a fost deschisa si data de altcineva

--- src/main/java/victor/training/java11/TryWithResources.java
+++ src/main/java/victor/training/java11/TryWithResources.java
@@ -9,9 +9,13 @@ import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.toList;
 
 public class TryWithResources {
-   public void checkFile(Stream<String> lines) {
-      if (lines.filter(s -> !s.isBlank()).count() <= 20) {
-         throw new IllegalArgumentException("Incorrect file!");
+   public void checkFile(Stream<String> lines) { // e considerat un pic fishy sa transm un Stream param. Daca-l consumi il termini
+      // un stream trebuie inchis daca se mapeaza pe linii de fisier sau resultseturi din DB
+      try (lines) { // dubios pentru ca inchizi o resursa deja creeata.
+         // Mai corect este ca de la creere sa si declari scope-ul dupa care se inchide
+         if (lines.filter(s -> !s.isBlank()).count() <= 20) {
+            throw new IllegalArgumentException("Incorrect file!");
+         }
       }
    }
 }
