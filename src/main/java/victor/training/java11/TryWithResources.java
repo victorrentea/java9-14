2b87f0385c1e75c7e8a634c2c11c598947f6c689
Victor Rentea <victorrentea@gmail.com>
2020-12-17 11:37:42 +0200

    best practice with closing resources: try with resource when you open the resource

--- src/main/java/victor/training/java11/TryWithResources.java
+++ src/main/java/victor/training/java11/TryWithResources.java
@@ -14,16 +14,18 @@ import static java.util.stream.Collectors.toList;
 public class TryWithResources {
    public void checkFile(Stream<String> lines) {
 
-      try (lines) { // try with existing resource
+//      try (lines) { // try with existing resource
          if (lines.filter(s -> !s.isBlank()).count() <= 20) {
             throw new IllegalArgumentException("Incorrect file!");
          }
-      }
+//      }
    }
 
    public static void main(String[] args) throws IOException {
       TryWithResources res = new TryWithResources();
 
-      res.checkFile(Files.lines(new File("a.txt").toPath()));
+      try (Stream<String> lines = Files.lines(new File("a.txt").toPath())) {
+         res.checkFile(lines);
+      }
    }
 }
