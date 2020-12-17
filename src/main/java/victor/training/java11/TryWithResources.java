608ad3d3bdc4915a76797674cc9f3f6d9c08922c
Victor Rentea <victorrentea@gmail.com>
2020-12-17 11:36:43 +0200

    try with existing resources

--- src/main/java/victor/training/java11/TryWithResources.java
+++ src/main/java/victor/training/java11/TryWithResources.java
@@ -1,7 +1,10 @@
 package victor.training.java11;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.nio.file.Files;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,8 +13,17 @@ import static java.util.stream.Collectors.toList;
 
 public class TryWithResources {
    public void checkFile(Stream<String> lines) {
-      if (lines.filter(s -> !s.isBlank()).count() <= 20) {
-         throw new IllegalArgumentException("Incorrect file!");
+
+      try (lines) { // try with existing resource
+         if (lines.filter(s -> !s.isBlank()).count() <= 20) {
+            throw new IllegalArgumentException("Incorrect file!");
+         }
       }
+   }
+
+   public static void main(String[] args) throws IOException {
+      TryWithResources res = new TryWithResources();
+
+      res.checkFile(Files.lines(new File("a.txt").toPath()));
    }
 }
