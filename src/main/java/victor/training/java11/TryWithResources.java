3594c8b479513d559f818e2adb050d19a37a75a9
Victor Rentea <victorrentea@gmail.com>
2020-12-17 11:41:51 +0200

    Pass in a Supplier<Resouce> that you then try with resource (java7 style) inside: again: keep opening the resource and closing it in the same method.

--- src/main/java/victor/training/java11/TryWithResources.java
+++ src/main/java/victor/training/java11/TryWithResources.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,20 +13,32 @@ import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.toList;
 
 public class TryWithResources {
-   public void checkFile(Stream<String> lines) {
+   public void checkFile(Supplier<Stream<String>> linesSupplier) {
 
-//      try (lines) { // try with existing resource
+      try (Stream<String> lines = linesSupplier.get()) {
          if (lines.filter(s -> !s.isBlank()).count() <= 20) {
             throw new IllegalArgumentException("Incorrect file!");
          }
-//      }
+      }
+
+      try (Stream<String> lines = linesSupplier.get()) {
+         if (lines.count() > 10000) {
+            throw new IllegalArgumentException();
+         }
+      }
    }
 
    public static void main(String[] args) throws IOException {
       TryWithResources res = new TryWithResources();
 
-      try (Stream<String> lines = Files.lines(new File("a.txt").toPath())) {
-         res.checkFile(lines);
+      res.checkFile(TryWithResources::f);
+   }
+
+   public static Stream<String> f() {
+      try {
+         return Files.lines(new File("a.txt").toPath());
+      } catch (IOException e) {
+         throw new RuntimeException(e);
       }
    }
 }
