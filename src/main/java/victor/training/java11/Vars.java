06da3c7793eacba6a659aa1485753de4016921bb
Victor Rentea <victorrentea@gmail.com>
2020-12-17 10:51:41 +0200

    vars

--- src/main/java/victor/training/java11/Vars.java
+++ src/main/java/victor/training/java11/Vars.java
@@ -1,13 +1,13 @@
 package victor.training.java11;
 
 
-
-import java.util.Arrays;
-import java.util.HashMap;
+import java.io.File;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static java.util.Map.entry;
+import static java.util.Map.ofEntries;
 
 public class Vars {
 
@@ -20,14 +20,63 @@ public class Vars {
 
    // var for lambda args, for annotations; all or none
 
+   public void var() {
+
+   }
+
+   public static final Map<Integer, List<String>> MAP = ofEntries(
+       entry(1, List.of("one")),
+       entry(2, List.of("one", "two")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr")),
+       entry(3, List.of("o", "t", "thr"))
+   );
+
+
    public static void main(String[] args) {
       // TODO Map.of(), .ofEntries()
-      Map<Integer, List<String>> map = new HashMap<>();
-      map.put(1, Arrays.asList("one"));
-      map.put(2, Arrays.asList("one", "two"));
+//      Set.of(1, 2, 4);
 
-      for (Entry<Integer, List<String>> entry : map.entrySet()) {
+      var x = 1;  // LOCAL VARIABLE (not params, not fiels) TYPE INFERENCE (not dynamic typing phew!!!)
+//      x = 2;
+      // var in JS
+//      x = "a";
+
+
+      var badName = theMethod();
+
+      for (var entry : badName.entrySet()) {
          System.out.println(entry.getKey());
       }
+
+      //      File file = new File("a.txt");
+//      try (Stream<List<String>> lineCells = openLineCellStream(file)) {
+//         lineCells.forEach(Vars::handle);
+//      }
+
+//      RestTemplate ;
+//
+//      /*ResponseEntity<List<Whatever>>*/ var response = exchange("htt", new TypeReference<>)
+//       response.getEntity().getBody()
    }
+
+   private static void handle(List<String> strings) {
+
+   }
+
+   public static Map<Integer, List<String>> theMethod() {
+      return MAP;
+   }
+//   var i = 1;
+//   public void method(var i) {
+//   }
 }
