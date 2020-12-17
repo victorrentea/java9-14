5c22aff90fb868b7aa60bd30e499a1543be15c2a
Victor Rentea <victorrentea@gmail.com>
2020-12-17 12:26:10 +0200

    enhanced switch

--- src/main/java/victor/training/java14/Switch.java
+++ src/main/java/victor/training/java14/Switch.java
@@ -6,37 +6,65 @@ import org.slf4j.LoggerFactory;
 public class Switch {
    private static final Logger log = LoggerFactory.getLogger(Switch.class);
    public static void main(String[] args) {
-      System.out.println("VAT BG = " + getVAT("BG", 10, true));
-      System.out.println("VAT BD = " + getVAT("MD", 20, false));
+      System.out.println("VAT BG = " + getVAT(Country.BG, 10, true));
+      System.out.println("VAT BD = " + getVAT(Country.MD, 20, false));
 
       auditMessage("CreateOrder");
    }
 
-   private static double getVAT(String isoCode, double value, boolean tobacco) {
-      /* TODO for MD return 5 + ... if tobacco==true */
-      switch (isoCode) {
-         case "BG":
-            return 0;
-         case "US":
-         case "MX":
-            return 15 + .05 * value;
-         case "MD":
-            return 7 + .02 * value;
-         default:
-            throw new IllegalArgumentException();
-      }
+   enum Country{
+      BG, US, MX, MD,
+      NL
+   }
+
+   private static double getVAT(Country isoCode, double value, boolean tobacco) {
+      /* TODO for BG return 5 + ... if tobacco==true */
+//      int x;
+//      if (isoCode ==Country.BG) {
+//         x = 2;
+//      }
+//      return x;
+       return switch (isoCode) {
+         case BG -> {
+            if (tobacco) {
+               yield 5 + 4;
+            }
+            yield 5;
+         }
+         case US, MX -> computeNonEuropeanTaxes(value);
+         case MD -> computeMoldavianTaxes(value);
+          case NL -> 2;
+//         default -> throw new IllegalStateException("HOPE; Unexpected value: " + isoCode);
+       };
+   }
+
+   private static double computeMoldavianTaxes(double value) {
+      return 7 + .02 * value;
+   }
+
+   private static double computeNonEuropeanTaxes(double value) {
+      // John had to put 3 lines of code somewhere, so why not here
+      // John had to put 3 lines of code somewhere, so why not here
+      // John had to put 3 lines of code somewhere, so why not here
+
+      // Maria had to put 7 lines of code somewhere, so why not here
+      // Maria had to put 7 lines of code somewhere, so why not here
+      // Maria had to put 7 lines of code somewhere, so why not here
+      // Maria had to put 7 lines of code somewhere, so why not here
+      // Maria had to put 7 lines of code somewhere, so why not here
+      // Maria had to put 7 lines of code somewhere, so why not here
+      // Maria had to put 7 lines of code somewhere, so why not here
+
+      return 15 + .05 * value;
    }
 
    public static void auditMessage(String messageCode) {
       switch (messageCode) {
-         case "CreateOrder":
-            log.info("Order Created");
-            break;
+         case "CreateOrder" ->
+             log.info("Order Created");
 
-         case "ViewOrder":
-         case "PrintOrder":
+         case "ViewOrder", "PrintOrder" ->
             log.info("Order Accessed");
-            break;
       }
    }
 }
