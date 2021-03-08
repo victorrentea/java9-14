0d5279fc9cf4e9b0ae86434297a2dc0719c08baa
Victor Rentea <victorrentea@gmail.com>
2021-03-08 11:23:03 +0200

    Records

--- src/main/java/victor/training/java14/Records.java
+++ src/main/java/victor/training/java14/Records.java
@@ -1,55 +1,69 @@
 package victor.training.java14;
 
-import java.util.Objects;
+import static java.lang.Integer.parseInt;
 
 public class Records {
    public static void main(String[] args) {
+
+      System.out.println(new Interval(1, 1).equals(new Interval(1, 1)));
+
+      Interval interval = new Interval(1, 2);
+
+      interval = interval.moveRight(1);
+
+      Interval overLoad = new Interval("1", "0");
+
+
+//      Interval nuEOk = new Interval(1, 0); // exceptie
+
       // final final final
       // implements, abstract and extends
       // 2nd constructor
       // extra content
       // override generated methods
-   }
 
-   public static boolean intervalsIntersect(Interval interval1, Interval interval2) {
-      return interval1.getStart() <= interval2.getEnd() && interval2.getStart()<= interval1.getEnd();
+
+      //Profetie: In java 23 vei putea face asa:
+
+      // ca in JS,   TS , py, Scala, Kotlin
+//      Interval(int x, int y) = overload;
    }
 }
-class Interval {
-   private final int start;
-   private final int end;
 
-   Interval(int start, int end) {
-      this.start = start;
-      this.end = end;
+
+record Interval(int start, int end) implements Comparable<Interval>{
+   // hibernate nu va suporta vreodata asa ceva entityManager.getReference();
+   // Jackson face eforturi - va merge
+   // JAXB nu cred ca va merge prea curand (WSDL, XSD)
+   // Mongo va suporta asa cum suporta deja Immutables (librarie)
+
+   public Interval {
+      if (start > end) throw new IllegalArgumentException();
    }
 
-   public int getEnd() {
-      return end;
+   public Interval(String startStr, String endStr) {
+      this(parseInt(startStr), parseInt(endStr));
    }
 
-   public int getStart() {
-      return start;
+   public int endExclusive() {
+      return end + 1;
+   }
+
+   public boolean intersects(Interval other) {
+      return start <= other.end && other.start<= end;
+   }
+
+   public Interval moveRight(int delta) {
+      return new Interval(start + delta, end + delta);
+   }
+
+   public float center() {
+      return (start + end) / 2.0f;
    }
 
    @Override
-   public String toString() {
-      return "Interval{" +
-             "start=" + start +
-             ", end=" + end +
-             '}';
-   }
-
-   @Override
-   public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      Interval interval = (Interval) o;
-      return start == interval.start && end == interval.end;
-   }
-
-   @Override
-   public int hashCode() {
-      return Objects.hash(start, end);
+   public int compareTo(Interval o) {
+      return Float.compare(center(), o.center());
    }
 }
+
