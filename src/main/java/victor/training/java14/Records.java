550a09827b7ad4851c004ffabb7d788de32be360
Victor Rentea <victorrentea@gmail.com>
2021-03-08 09:53:19 +0200

    clase imutabile mici, pe stilu vechi.

--- src/main/java/victor/training/java14/Records.java
+++ src/main/java/victor/training/java14/Records.java
@@ -1,5 +1,7 @@
 package victor.training.java14;
 
+import java.util.Objects;
+
 public class Records {
    public static void main(String[] args) {
       // final final final
@@ -9,7 +11,45 @@ public class Records {
       // override generated methods
    }
 
-   public static boolean intervalsIntersect(int start1, int end1, int start2, int end2) {
-      return start1 <= end2 && start2 <= end1;
+   public static boolean intervalsIntersect(Interval interval1, Interval interval2) {
+      return interval1.getStart() <= interval2.getEnd() && interval2.getStart()<= interval1.getEnd();
+   }
+}
+class Interval {
+   private final int start;
+   private final int end;
+
+   Interval(int start, int end) {
+      this.start = start;
+      this.end = end;
+   }
+
+   public int getEnd() {
+      return end;
+   }
+
+   public int getStart() {
+      return start;
+   }
+
+   @Override
+   public String toString() {
+      return "Interval{" +
+             "start=" + start +
+             ", end=" + end +
+             '}';
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Interval interval = (Interval) o;
+      return start == interval.start && end == interval.end;
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(start, end);
    }
 }
